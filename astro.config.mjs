12a6f7910e1445fce55eec102fa3c3e99f63c40d
mauricio <tutancabron10001@gmail.com>
2026-01-22 13:08:57 -0500

    fix: update vercel adapter and config for serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: false }
+  }),
   vite: {
     ssr: {
       external: ['@libsql/client']
