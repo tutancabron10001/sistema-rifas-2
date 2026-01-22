9a21cd759ed59d220742dfe68d0cfa1001fa0e45
mauricio <tutancabron10001@gmail.com>
2026-01-22 13:27:11 -0500

    fix: downgrade adapter to 7.7.0 and set nodejs20.x runtime

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
   adapter: vercel({
-    webAnalytics: { enabled: false }
+    runtime: 'nodejs20.x'
   }),
   vite: {
     ssr: {
